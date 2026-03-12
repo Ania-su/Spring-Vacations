f85d533c706e97ed91c0a85f1a079dc97f13d3ab
Ania-su <antsaaa.randria@gmail.com>
2026-03-12 09:15:41 +0300

    feat: project init

---
+++ src/main/java/org/example/springvacations/SpringVacationsApplication.java
@@ -0,0 +1,13 @@
+package org.example.springvacations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVacationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVacationsApplication.class, args);
+    }
+
+}
